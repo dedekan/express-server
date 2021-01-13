50c87bcb5bae3a51be70b232d83431d41d2e7f1c
Amral Nazar <amral.nazar@gmail.com>
2021-01-13 15:22:09 +0700

    fix: apply content type for image response

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const axios = require('axios');
 const cors = require('cors');
 const https = require('https');
 const _ = require('lodash');
+const { URL } = require('url');
 
 const api = axios.create({
   httpsAgent: new https.Agent({
@@ -17,25 +18,33 @@ app.use(cors());
 // Create GET request
 app.get('/', (req, res) => {
   const _url = req.query._url;
-  const params = _.omit(req.query, ['_url', '_contentType']);
+  const params = _.omit(req.query, ['_url', '_contentType', '_isImage']);
   const _contentType = req.query._contentType;
+  const _isImage = req.query._isImage;
 
-  const headers = {};
+  const otherParams = {};
+
   if (_contentType) {
-    headers['Content-Type'] = _contentType;
+    otherParams.headers = {
+      'Content-Type': _contentType
+    };
+  }
+
+  if (_isImage) {
+    otherParams.responseType = 'arraybuffer';
   }
 
   api
     .get(_url, {
-      params,
-      headers
+      ...otherParams,
+      params
     })
     .then(response => {
-      if (_contentType === 'image/png') {
-        res.sendFile(response.data);
-      } else {
-        res.send(response.data);
+      if (_contentType) {
+        res.contentType(_contentType);
       }
+
+      res.send(response.data);
     })
     .catch(err => {
       res.send(err);
